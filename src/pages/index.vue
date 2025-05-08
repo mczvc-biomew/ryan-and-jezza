fe01f7b08db4cbb2b14e9f53e5d4122ccec5b72e
Meldencio Czarlemagne <mczvc@proton.me>
2025-05-08 16:59:58 +0800

    add SEO

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,6 +5,25 @@
   </div>
 </template>
 
+<script setup lang="ts">
+const route = useRoute();
+
+const getSrc = (path: string): string => {
+  const modules = import.meta.glob("/assets/**/*", { eager: true});
+  const file = <{ default: string }>(modules[path]) || {default: ''};
+
+  return file.default || '';
+}
+
+useSeo({
+  type: 'WebSite',
+  location: route.path,
+  icon: getSrc("/assets/favicon.ico"),
+  useConfig: true
+});
+
+</script>
+
 <style lang="less">
 body {
   @apply m-0;
