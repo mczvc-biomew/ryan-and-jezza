6118cf4337ca458c98d8a3976f11e03386de7c75
Meldencio Czarlemagne <mczvc@proton.me>
2025-05-08 11:59:56 +0800

    add app baseUrl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
   srcDir: 'src/',
+  app: {
+    baseURL: '/ryan-and-jezza'
+  },
   modules: ['@unocss/nuxt', '@nuxtjs/i18n', '@vueuse/nuxt', '@nuxtjs/mdc'],
   css: [ '~/assets/css/fonts.css' ],
   postcss: {
