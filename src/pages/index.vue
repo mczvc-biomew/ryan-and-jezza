284d51250293c6a158d7b9bc9de50e04ea5e1e57
Meldencio Czarlemagne <mczvc@proton.me>
2025-05-09 09:55:05 +0800

    refactor

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -15,13 +15,40 @@ const getSrc = (path: string): string => {
   return file.default || '';
 }
 
-// useSeo({
-//   type: 'WebSite',
-//   location: route.path,
-//   icon: getSrc("/assets/favicon.ico"),
-//   useConfig: true
-// });
-useTitle('Ryan Christian and Jezza are Getting Married!');
+if (import.meta.server) {
+  useSeo({
+    type: 'WebSite',
+    location: route.path,
+    icon: getSrc("/assets/favicon.ico"),
+    useConfig: true
+  });
+}
+
+function init() {
+
+  const revealOnScrollObserver = new IntersectionObserver((entries) => {
+    entries.forEach( (entry) => {
+      if (entry.isIntersecting)
+        entry.target.classList.add('reveal');
+    });
+  }, { threshold: 0.5});
+  Array.from(document.querySelectorAll('.animate-on-scroll')).forEach( (el) => {
+    revealOnScrollObserver.observe(el);
+  });
+
+  let timeSequence = 1000;
+  const animateOnce = document.querySelectorAll('.animate-once');
+  animateOnce.forEach( (el) => {
+    setTimeout( () => {
+      el.classList.add('reveal');
+    }, timeSequence);
+    timeSequence += 500;
+  });
+}
+
+onMounted( () => {
+  init();
+});
 
 </script>
 
@@ -29,7 +56,53 @@ useTitle('Ryan Christian and Jezza are Getting Married!');
 body {
   @apply m-0;
 }
-// .page-container {
-// }
 
+.animate-reveal-up(@translate-y) {
+  transition: transform ease 1s, opacity ease 800ms;
+  transform: translate3d(0, @translate-y, 0);
+  opacity: 0;
+
+  &.reveal {
+    transform: translate3d(0, 0, 0);
+    opacity: 1;
+  }
+}
+
+.animate-spin-y(@y-deg) {
+  transition: transform ease 1s, opacity ease 800ms;
+  --translate-x: 40vw;
+  --rotate-y: @y-deg;
+  transform: translateX(var(--translate-x)) rotateY(var(--rotate-y));
+  opacity: 0;
+
+  &.reveal {
+    --rotate-y: 0;
+    --translate-x: 50vw;
+    opacity: 1;
+  }
+}
+
+.animate-slide-left(@x) {
+  transition: transform ease 500ms, opacity ease 400ms;
+  --translate-x: @x;
+  transform: translateX(var(--translate-x));
+  opacity: 0;
+
+  &.reveal {
+    --translate-x: 0;
+    opacity: 1;
+  }
+}
+
+.animate-slide-left-50 {
+  .animate-slide-left(30vw);
+}
+
+.animate-spin-y {
+  .animate-spin-y(720deg);
+}
+
+.animate-reveal-up-50 {
+  .animate-reveal-up(50px);
+}
 </style>
